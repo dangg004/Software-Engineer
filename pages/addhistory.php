96ed041430a558d1efb579b28da645a53b38294b
Mizuwawa <thinhronando2134@gmail.com>
2024-12-05 20:18:14 +0700

    File handle giao tiếp với SQL để gửi lịch sử in lên

--- pages/addhistory.php
+++ pages/addhistory.php
@@ -1,17 +1,30 @@
 <?php 
+//Mở session
 session_start();
 
+//Nhận dữ liệu được gửi từ nguồn gọi file này dậy (ở đây là cái nút "In")
 $received1 = $_POST['printer'];
 $received2 = $_POST['location'];
 $received3 = $_POST['date'];
+
+//Dữ liệu tạm thời chờ đồng bộ
 $user = "user1";
 $file = "sample.txt";
-// Create connection
+
+// Kết nối tới MySQL của XMAPP
 $conn = mysqli_connect("localhost", 'root', '', 'userinfo');
+
+//Đếm số lượng dữ liệu đã có sẵn dể set ID (Có thể gặp lỗi, để handle sau)
 $sql1 = "SELECT count(*) as id from history";
+//Gọi sql
 $res = mysqli_query($conn, $sql1);
+//Lấy dữ liệu được sql trả về
 $id = mysqli_fetch_assoc($res)["id"];
+
+//Thêm một hàng mới vào history
 $sql2 = "INSERT INTO history(hisid, uploadedUser, printerUsed, printerLocation, printDate, printedFile) VALUES (?,?,?,?,?,?)";
 $conn->execute_query($sql2, [$id,$user, $received1, $received2, $received3, $file]);
+
+//tắt session
 session_abort();
 ?>
