b45b8c707a67f8f53d81988126d39f3f662f5822
WtfIsAKilometer <168650815+WtfIsAKilometer@users.noreply.github.com>
2024-12-07 00:06:07 +0700

    Add files via upload

--- pages/sso.php
+++ pages/sso.php
@@ -15,7 +15,20 @@ $result = mysqli_query($con, $sql);
 
 if(mysqli_num_rows($result) >= 1){
     $_SESSION["myuser"] = $finaluser;
-    $link = "Location: /pages/index.html?username=" . $finaluser . "&" . "password=" . $finalpass;
+    
+    $sql = "SELECT * FROM user_balance where username = '$finaluser' AND password = '$finalpass'";
+    $result = mysqli_query($con, $sql);
+    if(mysqli_num_rows($result) < 1)
+    {
+        $sql = "INSERT INTO user_balance (username,password,normalPaper,colorPaper) values ('$finaluser','$finalpass',0,0)";
+        $result = mysqli_query($con, $sql);
+    }
+    $sql = "SELECT normalPaper FROM user_balance where username = '$finaluser' AND password = '$finalpass'";
+    $normalPaper=mysqli_fetch_array(mysqli_query($con, $sql))['normalPaper'];
+    $sql = "SELECT colorPaper FROM user_balance where username = '$finaluser' AND password = '$finalpass'";
+    $colorPaper=mysqli_fetch_array(mysqli_query($con, $sql))['colorPaper'];
+
+    $link = "Location: /pages/index.html?username=" . $finaluser . "&" . "password=" . $finalpass. "&" . "nn=" .$normalPaper. "&" . "cc=" .$colorPaper;
     header($link);
 }else{
     $_SESSION["error"] = "You are not valid user";
